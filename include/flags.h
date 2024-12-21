6017fb9d8eff22d302703329e3e6b9c507b46199
Theodore Cooper <63190431+the0cp@users.noreply.github.com>
2024-12-21 16:10:43 +0800

    Update flags.h

--- include/flags.h
+++ include/flags.h
@@ -2,6 +2,6 @@
 #define FLAG_IS_DST false
 #define FLAG_DEFAULT_ISO_8601_TIME true
 #define FLAG_DEFAULT_WITH_MILLISEC false
-#define ENABLE_CONSOLE_OUT true
+#define ENABLE_CONSOLE_OUT false
 #define FLAG_LOG_DIR "/tmp/"
 #define FLAG_LOG_NAME "log"
